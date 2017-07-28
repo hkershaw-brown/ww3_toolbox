e0b0897d1fa11a55a164ceae00e3927ef36d417a
qingli411 <lqingpku@gmail.com>
2017-07-28 16:42:42 -0400

    Add fit curve in plot_efactor_miz.m

--- analysis/plot_efactor_miz.m
+++ analysis/plot_efactor_miz.m
@@ -6,8 +6,10 @@
 clear variables; close all;
 
 % add path of share directory
-[s, r] = system('echo ${WW3_TOOLBOX_ROOT}');
-path([r(1:end-1) '/share'], path);
+% [s, r] = system('echo ${WW3_TOOLBOX_ROOT}');
+% path([r(1:end-1) '/share'], path);
+pShare = '../share';
+path(pShare, path);
 
 % set case name and directory
 pData = '/Users/qingli/data_local/WW3';
@@ -24,13 +26,22 @@ ice = ncread(inFile2, 'ice');
 lat = ncread(inFile2, 'latitude');
 time = ncread(inFile2, 'time');
 nt = numel(time);
-efC = 2.5;	% set a cap on efactor, will affect the mean
+
+% expand lat array
+latall = zeros(size(efactor));
+for i=1:nt
+    latall(:,:,i) = lat;
+end
+
+efC = 5;	% set a cap on efactor, will affect the mean
 efactor(efactor >= efC) = efC;
 dl = 0.05;  % ice interval
+icutoff = 0.01;
 iceLev = 0:dl:1;
 iceMin = iceLev(1:end-1);
 iceMax = iceLev(2:end);
 ni = numel(iceLev);
+iceMin(1) = icutoff;
 
 % percentiles
 pct = [10, 25, 50, 75, 90];
@@ -43,7 +54,7 @@ ndat = zeros([1,ni]);
 % loop over ice fraction
 for i=1:ni
 	if i == 1
-		inds = find(ice == 0);
+		inds = find(ice < icutoff);
 	else
 		inds = find(ice > iceMin(i-1) & ice <= iceMax(i-1));
 	end
@@ -60,7 +71,7 @@ lstyle = {'-.','--','-','--','-.'};
 figure;
 hold on;
 % x-axis: ice fraction
-xx = [0, 0.5.*(iceMin+iceMax)];
+xx = [0.01, 0.5.*(iceMin+iceMax)];
 % number of points in arbitrary scale
 ndat2 = ndat;
 ndat2(1) = NaN;
@@ -80,6 +91,18 @@ xlabel('Ice fraction');
 ylabel('Enhancement factor');
 xlim([0,1]);
 ylim([1,1.8]);
+
+% fit data
+% y = 1+beta*exp((1-sqrt(x))/alpha)
+fxx = xx(3:end).^(0.5);
+fyy = log(datmn(3:end)-1);
+c = fit(fxx', fyy', 'poly1');
+alpha = -1/c.p1;
+beta = exp(c.p2+c.p1);
+yy = beta.*exp((1-fxx)./alpha)+1;
+plot(xx(3:end), yy, '-c');
+
+% set(gca,'xscale','log');
 % save figure
 figname = [cName '_efactor_prct.fig'];
 saveas(gcf, figname, 'fig');
